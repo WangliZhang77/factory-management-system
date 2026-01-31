7511bde983c7a9434d7f5c5a34a508aacc6de314
Wangli Zhang <zwled1014@gmail.com>
2026-01-31 23:13:03 +1300

    Add completion and cancellation fields to WorkOrder entity

---
+++ FactoryManagementSystem1/Migrations/20260131101140_AddWorkOrderCompletionAndCancellationFields.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactoryManagementSystem1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkOrderCompletionAndCancellationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CancelledAtUtc",
+                table: "WorkOrders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CancelledByUserId",
+                table: "WorkOrders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CompletedAtUtc",
+                table: "WorkOrders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompletedByUserId",
+                table: "WorkOrders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledAtUtc",
+                table: "WorkOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CancelledByUserId",
+                table: "WorkOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CompletedAtUtc",
+                table: "WorkOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CompletedByUserId",
+                table: "WorkOrders");
+        }
+    }
+}
